80caffe527ff08b39f88d8210681f742ae82dc29
krsdl <35543755+Kresdl@users.noreply.github.com>
2020-08-03 22:02:28 +0200

    renames and comments

--- index.ts
+++ index.ts
@@ -27,9 +27,11 @@ const pgfx = prev.getContext('2d') as CanvasRenderingContext2D
 const scoreEm = document.getElementById('score') as HTMLParagraphElement
 scoreEm.innerText = '0'
 
-const heap = new Uint8Array(WIDTH * HEIGHT);
+const pile = new Uint8Array(WIDTH * HEIGHT);
 
-const vec = (size: number, ori: number): number[] => {
+// Get sample model for array-like square grid with specific orientation as 
+// [offset, delta with respect to game x, delta with respect to game y]
+const offsetAndDeltas = (size: number, ori: number): number[] => {
   const rgt = size - 1
   const btm = size * (size - 1)
 
@@ -41,6 +43,7 @@ const vec = (size: number, ori: number): number[] => {
   ][ori]
 }
 
+// Randomize piece and x position
 const generatePiece = (): State => {
   const id = ~~(Math.random() * pieces.length)
   const size = pieces[id][0]
@@ -51,26 +54,29 @@ const generatePiece = (): State => {
   }
 }
 
-const scan = (top: number): number => {
+// Check for full rows and remove them
+const scanAndClearRows = (top: number): number => {
   const cols = [...Array(WIDTH)].map((_, i) => i)
   const rows = [...Array(HEIGHT - top)].map((_, i) => i + top)
 
   const full = rows.find(row =>
-    cols.every(col => heap[col + WIDTH * row])
+    cols.every(col => pile[col + WIDTH * row])
   )
 
   if (!full) return 0
-  heap.copyWithin(top * WIDTH, (top - 1) * WIDTH, full * WIDTH)
+  pile.copyWithin(top * WIDTH, (top - 1) * WIDTH, full * WIDTH)
 
   const snap = gfx.getImageData(0, SCALE * (top - 1), SCALE * WIDTH, SCALE * (full - top + 1))
   gfx.putImageData(snap, 0, SCALE * top)
 
-  return scan(top + 1) + 1
+  return scanAndClearRows(top + 1) + 1
 }
 
-const insertPiece = (piece: State, top?: number | null): number | boolean => {
+// Merge piece with pile or check for fit
+const insertIntoDataModel = (piece: State, dryrun?: boolean): number | boolean => {
   const [size, pattern] = pieces[piece.id]
-  const m = vec(size, piece.ori)
+  const m = offsetAndDeltas(size, piece.ori)
+  let minY = HEIGHT
 
   for (let y = 0; y < size; y++) {
     const yc = piece.y + y
@@ -82,26 +88,28 @@ const insertPiece = (piece: State, top?: number | null): number | boolean => {
         src = pattern[uv],
         outside = yc >= HEIGHT || xc < 0 || xc >= WIDTH
 
-      if (top) {
-        if (src && !outside && yc >= 0) {
-          heap[xy] = 1
-          top = Math.min(top, yc)
+      if (dryrun) {
+        if (src && (outside || pile[xy])) {
+          return false
         }
-      } else if (src && (outside || heap[xy])) {
-        return false
+      } else if (src && !outside && yc >= 0) {
+        pile[xy] = 1
+        minY = Math.min(minY, yc)
       }
     }
   }
-  return typeof top !== 'number'
-    ? true : top
+
+  return dryrun ? true : minY
 }
 
-const bottom = (piece: State): State => {
+// Get pile merge y for piece 
+const bottom = (piece: State): number => {
   let d = 0
-  while (insertPiece({ ...piece, y: piece.y + d + 1 })) d++
-  return { ...piece, y: piece.y + d }
+  while (insertIntoDataModel({ ...piece, y: piece.y + d + 1 }, true)) d++
+  return piece.y + d
 }
 
+// Render piece
 const drawPiece = (piece: State, alpha: number = 1, rule: string = 'source-over') => {
   const { id, x, y, ori } = piece
   const img = bitmaps[id]
@@ -114,11 +122,13 @@ const drawPiece = (piece: State, alpha: number = 1, rule: string = 'source-over'
   gfx.drawImage(img, 0, 0)
 }
 
+// Render preview
 const drawPreview = (piece: State) => {
   pgfx.clearRect(0, 0, 4 * SCALE, 4 * SCALE)
   pgfx.drawImage(bitmaps[piece.id], 0, 0)
 }
 
+// Control scope
 const main = async () => {
   bitmaps = await Promise.all(
     pieces.map(([size, array, color]) => {
@@ -139,58 +149,66 @@ const main = async () => {
   )
 
   let tile = generatePiece()
-  let guideTile = bottom(tile)
+  let guideTile = { ...tile, y: bottom(tile) }
   let next = generatePiece()
   let top = HEIGHT
   let score = 0
 
+  // Sideway-movements and rotation
   const move = (piece: State) => {
-    if (insertPiece(piece)) {
+    if (insertIntoDataModel(piece, true)) {
       drawPiece(tile, 1, 'xor')
       drawPiece(guideTile, 1, 'destination-out')
-      guideTile = bottom(piece)
+      guideTile = { ...piece, y: bottom(piece) }
       drawPiece(guideTile, 0.25)
       drawPiece(piece)
       tile = piece
     }
   }
 
+  // Downward movement
   const down = () => {
     const dst = { ...tile, y: tile.y + 1 }
 
-    if (insertPiece(dst)) {
+    if (insertIntoDataModel(dst, true)) {
       drawPiece(tile, 1, 'xor')
       drawPiece(dst)
       tile = dst
 
     } else {
-      attach()
+      mergePiece()
     }
   }
 
-  const toBottom = () => {
-    const dst = bottom(tile)
+  // Drop to bottom
+  const drop = () => {
+    const dst = { ...tile, y: bottom(tile) }
     drawPiece(tile, 1, 'xor')
     drawPiece(dst)
     tile = dst
-    attach()
+    mergePiece()
   }
 
-  const attach = () => {
-    top = insertPiece(tile, top) as number
+  // Merge piece. Called by down() and drop() 
+  const mergePiece = () => {
+    const minY = insertIntoDataModel(tile) as number
 
-    if (!top) {
+    // All piled up?
+    if (!minY) {
       clearInterval(interval)
 
     } else {
-      const count = scan(top)
+      top = Math.min(top, minY)
+      const count = scanAndClearRows(top)
+
+      // Cleared rows?
       if (count) {
         score += POINTS[count - 1]
         scoreEm.innerText = score.toString()
         top += count
       }
       tile = next
-      guideTile = bottom(tile)
+      guideTile = { ...tile, y: bottom(tile) }
       next = generatePiece()
       drawPiece(guideTile, 0.25)
       drawPreview(next)
@@ -225,7 +243,7 @@ const main = async () => {
         break
       }
       case 'Space': {
-        toBottom()
+        drop()
         break
       }
       default:
